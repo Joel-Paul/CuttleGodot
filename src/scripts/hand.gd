ea5cb3fe5ffe62d4f0296d2aee66260cbaa91524
Joel Paul <joel.v.paul@outlook.com>
2024-01-02 22:03:50 +1100

    Improve hand code

--- src/scripts/hand.gd
+++ src/scripts/hand.gd
@@ -1,23 +1,44 @@
 @tool
 extends Node2D
 
-@export var curvature: float = 4:
+@export_range(0, 20) var max_card_expansion: int = 8:
 	set(val):
-		curvature = val
-		queue_redraw()
-@export_range(0, 5) var max_spacing: float = 1:
-	set(val):
-		max_spacing = val
-		queue_redraw()
-@export_range(0, 0.1) var spacing_scale: float = 0.06:
-	set(val):
-		spacing_scale = val
-		queue_redraw()
-@export_range(0, 10) var rotation_scale: float = .4:
-	set(val):
-		rotation_scale = val
+		max_card_expansion = val
 		queue_redraw()
 
+#region Hand scaling
+@export_group("Scaling", "scale")
+@export_range(0, 5) var scale_spacing_x: float = 2.5:
+	set(val):
+		scale_spacing_x = val
+		queue_redraw()
+@export_range(0, 5) var scale_spacing_y: float = .5:
+	set(val):
+		scale_spacing_y = val
+		queue_redraw()
+@export_range(0, PI / 2) var scale_rotation: float = .25:
+	set(val):
+		scale_rotation = val
+		queue_redraw()
+#endregion
+
+#region Hand curves
+@export_group("Curves", "curve")
+@export var curve_spacing_x: Curve = preload("res://src/curves/hand_spacing_x.tres"):
+	set(val):
+		curve_spacing_x = val
+		queue_redraw()
+@export var curve_spacing_y: Curve = preload("res://src/curves/hand_spacing_y.tres"):
+	set(val):
+		curve_spacing_y = val
+		queue_redraw()
+@export var curve_rotation: Curve = preload("res://src/curves/hand_rotation.tres"):
+	set(val):
+		curve_rotation = val
+		queue_redraw()
+#endregion
+
+#region Hand preview
 @export_group("Preview", "preview")
 @export var preview_show_cards: bool = true:
 	set(val):
@@ -31,43 +52,45 @@ extends Node2D
 	set(val):
 		preview_card_texture = val
 		queue_redraw()
-@export_range(0, 20) var preview_num_cards: int = 6:
+@export_range(0, 20) var preview_num_cards: int = 8:
 	set(val):
 		preview_num_cards = val
 		queue_redraw()
+#endregion
 
 func _draw() -> void:
 	if Engine.is_editor_hint():
 		_draw_preview()
 
 func _draw_preview() -> void:
-	var offset = Global.CARD_SIZE / -2.0  # make centre of card the origin
-	var points = PackedVector2Array()
-	
-	for i in preview_num_cards:
-		var trans = get_card_transform(i, preview_num_cards)
-		draw_set_transform_matrix(trans)
+	if preview_show_cards:
+		var offset = Global.CARD_SIZE / -2.0  # make centre of card the origin
+		for i in preview_num_cards:
+			var trans = get_card_transform(i, preview_num_cards)
+			draw_set_transform_matrix(trans)
 
-		var rect = Rect2(offset, Global.CARD_SIZE)
-		if preview_show_cards:
+			var rect = Rect2(offset, Global.CARD_SIZE)
 			draw_texture_rect(preview_card_texture, rect, false)
-		
-		points.push_back(trans.get_origin())
+		draw_set_transform(Vector2.ZERO)
 	
-	draw_set_transform(Vector2.ZERO)
-	if preview_show_curve and points.size() > 1:
+	if preview_show_curve:
+		var points = PackedVector2Array()
+		for i in max_card_expansion:
+			var trans = get_card_transform(i, max_card_expansion)
+			points.push_back(trans.get_origin())
 		draw_polyline(points, Color.RED, 2)
 
 func get_card_transform(index: int, length: int) -> Transform2D:
-	# The reciprocal of the exponent is used to decrease the spacing between cards as the
-	# number of cards increase, preventing them from going off screen with a large amount
-	var spacing = max_spacing * exp(-length * spacing_scale)
-	# I don't remember how I derived the below equation
-	var x_pos = (index - (length - 1) / 2.0) * Global.CARD_SIZE.x * spacing
-	var y_pos = pow(x_pos, 2) * curvature / get_viewport_rect().size.x
-	var pos = Vector2(x_pos, y_pos)
+	# Effective length/index serves to "pad out" the hand
+	# when the number of cards is below `max_card_expansion`.
+	# Without this, the spacing between low amounts of cards is large.
+	var length_offset = 0 if length % 2 == max_card_expansion % 2 else 1
+	var effective_length = max(length, max_card_expansion - length_offset)
+	var effective_index = index + round((effective_length - length) / 2.0)
+	var norm_pos = effective_index / (effective_length - 1.0)
 	
-	# Derivative of y_pos (w.r.t. x_pos) * rotation_scale
-	var rot = atan(2 * x_pos * curvature / get_viewport_rect().size.x) * rotation_scale
+	var x_pos = curve_spacing_x.sample(norm_pos) * scale_spacing_x * Global.CARD_SIZE.x
+	var y_pos = -curve_spacing_y.sample(norm_pos) * scale_spacing_y * Global.CARD_SIZE.y
+	var rot = curve_rotation.sample(norm_pos) * scale_rotation
 	
-	return Transform2D(rot, pos)
+	return Transform2D(rot, Vector2(x_pos, y_pos))
